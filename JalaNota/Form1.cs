22d560ff06fd2d1968b658c5c1dbbf397701bed8
kayanazaria <kayana.azaria@gmail.com>
2025-09-06 15:13:35 +0700

    Refactor: Rename project and add .gitignore

--- JalaNota/Form1.cs
+++ JalaNota/Form1.cs
@@ -16,5 +16,10 @@ namespace Aplikasi_JalaNota
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
