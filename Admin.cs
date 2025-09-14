5e0bb64fbc29987c1d7a56fe338dd3a1f2238e1e
kayanaazaria <kayana.azaria@gmail.com>
2025-09-14 11:50:28 +0700

    Update class diagram for Admin.cs

--- Admin.cs
+++ Admin.cs
@@ -6,8 +6,58 @@ using System.Threading.Tasks;
 
 namespace JalaNota
 {
-    class Admin
+    public class Admin
     {
-        
+        // Properti
+        public int IDAdmin { get; set; }
+        public string NamaAdmin { get; set; }
+        public string UsernameAdmin { get; set; }
+        public string PasswordAdmin { get; set; }
+
+        // Metode
+        public Boolean Login(string username, string password)
+        {
+            if (username == "adminKay" && password == "kay123")
+            {
+                // TODO: ganti dummy dengan data dari database
+                IDAdmin = 1;
+                NamaAdmin = "Admin Kay";
+                UsernameAdmin = "adminKay";
+                return true;
+            }
+            else if (username == "adminEga" && password == "ega123")
+            {
+                IDAdmin = 2;
+                NamaAdmin = "Admin Ega";
+                UsernameAdmin = "adminEga";
+                return true;
+            }
+            else if (username == "adminNia" && password == "nia123")
+            {
+                IDAdmin = 3;
+                NamaAdmin = "Admin Nia";
+                UsernameAdmin = "adminNia";
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void TambahNelayan(string nama, string user, string password)
+        {
+            // TODO: isi kode untuk menyimpan data nelayan baru ke database
+            Console.WriteLine($"Menambahkan nelayan: Nama={nama}, Username={user}");
+        }
+        public void EditNelayan(int IDNelayan, string nama, string user, string password)
+        {
+            // TODO: isi kode untuk memperbarui data nelayan di database berdasarkan IDNelayan
+            Console.WriteLine($"Mengedit nelayan ID={IDNelayan}: Nama={nama}, Username={user}");
+        }
+        public void HapusNelayan(int IDNelayan)
+        {
+            // TODO: isi kode untuk menghapus data nelayan dari database berdasarkan IDNelayan
+        }
     }
 }
