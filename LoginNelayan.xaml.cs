2907049d25fd3cf40b9ed07aeb34257f2e70be81
kayanaazaria <kayana.azaria@gmail.com>
2025-10-26 17:34:22 +0700

    Tambah Login Page Nelayan

--- LoginNelayan.xaml.cs
+++ LoginNelayan.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace JalaNota
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for LoginNelayan.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class LoginNelayan : Window
     {
-        public MainWindow()
+        public LoginNelayan()
         {
             InitializeComponent();
         }
